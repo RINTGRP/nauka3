10ccdb7a93427dc16e0a8a660b40fe4027f42dbd
RINTGRP <rintgrp@gmail.com>
2019-05-02 21:49:46 +0200

    edit dots

---
+++ Nauka3script.js
@@ -0,0 +1,12 @@
+document.querySelector(".circlea").onClick=function(){
+    document.querySelector(".visible").classList.remove(".visible");
+    document.querySelector(".tomato").classList.add(".visible");
+}
+document.querySelector(".circleb").onClick=function(){
+    document.querySelector(".visible").classList.remove(".visible");
+    document.querySelector(".cucumber").classList.add(".visible");
+}
+document.querySelector(".circlec").onClick=function(){
+    document.querySelector(".visible").classList.remove(".visible");
+    document.querySelector(".carrot").classList.add(".visible");
+}
