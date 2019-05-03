c91b4e7be8a494f8cfcd636a30a2f61fdb2824b2
Daria Puchnarewicz <Daria.Puchnarewicz@stepstone.com>
2019-05-03 12:46:39 +0200

    fix onclick

--- Nauka3script.js
+++ Nauka3script.js
@@ -1,12 +1,14 @@
-document.querySelector(".circlea").onClick=function(){
-    document.querySelector(".visible").classList.remove(".visible");
-    document.querySelector(".tomato").classList.add(".visible");
+document.querySelector(".circlea").onclick=function(){
+    document.querySelector(".hidden").classList.remove("hidden");
+    document.querySelector(".tomato").classList.add("hidden");
+
+    console.log('clicked: ' + document.querySelector(".tomato").classList);
 }
-document.querySelector(".circleb").onClick=function(){
-    document.querySelector(".visible").classList.remove(".visible");
-    document.querySelector(".cucumber").classList.add(".visible");
+document.querySelector(".circleb").onclick=function(){
+    document.querySelector(".hidden").classList.remove("hidden");
+    document.querySelector(".cucumber").classList.add("hidden");
 }
-document.querySelector(".circlec").onClick=function(){
-    document.querySelector(".visible").classList.remove(".visible");
-    document.querySelector(".carrot").classList.add(".visible");
+document.querySelector(".circlec").onclick=function(){
+    document.querySelector(".hidden").classList.remove("hidden");
+    document.querySelector(".carrot").classList.add("hidden");
 }
