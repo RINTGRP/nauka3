452444d45214b4f08f3970c914e4e736aebc8ca5
RINTGRP <rintgrp@gmail.com>
2019-05-03 20:47:59 +0200

    end gallery buttons work

--- Naukascript.js
+++ Naukascript.js
@@ -1,12 +1,12 @@
 document.querySelector(".circlea").onclick=function(){
-    document.querySelector(".visible").classList.remove(".visible");
-    document.querySelector(".tomato").classList.add(".visible");
+    document.querySelector(".visible").classList.remove("visible");
+    document.querySelector(".tomato").classList.add("visible");
 }
 document.querySelector(".circleb").onclick=function(){
-    document.querySelector(".visible").classList.remove(".visible");
-    document.querySelector(".cucumber").classList.add(".visible");
+    document.querySelector(".visible").classList.remove("visible");
+    document.querySelector(".cucumber").classList.add("visible");
 }
 document.querySelector(".circlec").onclick=function(){
-    document.querySelector(".visible").classList.remove(".visible");
-    document.querySelector(".carrot").classList.add(".visible");
+    document.querySelector(".visible").classList.remove("visible");
+    document.querySelector(".carrot").classList.add("visible");
 }
